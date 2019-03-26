e0e9ec2f7cbe67dd360359f63ce4afead8424499
liuwenping <liuwenping828@gmail.com>
2019-03-26 21:25:07 +0800

    百度Orc SDK集成.

---
+++ orc_demo/src/main/java/com/lwp/recognition/FileUtil.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+package com.lwp.recognition;
+
+import java.io.File;
+
+import android.content.Context;
+
+public class FileUtil {
+
+    public static File getSaveFile(Context context) {
+        File file = new File(context.getFilesDir(), "pic.jpg");
+        return file;
+    }
+}
